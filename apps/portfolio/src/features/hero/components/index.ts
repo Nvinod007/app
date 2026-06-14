804c57ffa33102e406d12895cad840e35aa3633b
Nvinod007 <vinodkumar.nelanakula@gmail.com>
2026-06-14 13:34:45 +0530

    auto-commit for 17bdf002-9ec5-4822-b06f-19fd0d17d04b

--- apps/portfolio/src/features/hero/components/index.ts
+++ apps/portfolio/src/features/hero/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Hero } from "./Hero";
 export { default as ModernHero } from "./ModernHero";
+export { default as EnhancedHero } from "./EnhancedHero";
 export { default as TypingAnimation } from "./TypingAnimation";
 export { default as HeroCTAButtons } from "./HeroCTAButtons";
 export { default as HeroContactInfo } from "./HeroContactInfo";
--- apps/portfolio/src/shared/components/index.ts
+++ apps/portfolio/src/shared/components/index.ts
@@ -3,3 +3,6 @@ export { default as ParticleField } from "./ParticleField";
 export { default as CodeMatrix } from "./CodeMatrix";
 export { default as CursorEffect } from "./CursorEffect";
 export { default as WaveEffect } from "./WaveEffect";
+export { default as OptimizedBackground } from "./OptimizedBackground";
+export { default as ScrollProgress } from "./ScrollProgress";
+export { default as PerformanceMonitor } from "./PerformanceMonitor";
